39649dbf387237798d949c7df915e2de01579d53
TheAnimeEnthusiast <mritvik02@gmail.com>
2026-02-20 20:44:38 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# Write a program to check whether the given number is even or odd
+
+num = float(input("What number do you want to be determined whether it is even or odd?"))
+
+if num%2 == 0:
+
+    print("Your number is even")
+
+else:
+
+    print("Your number is odd")
